df57cfa26549edbb5e2cd69de92aa411312227d6
aaron <arnquest@hotmail.com>
2022-05-02 22:51:02 +0800

    unit test

---
+++ BinaryParseTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryParse;
+using System.Collections.Generic;
+
+namespace BinaryParseTest
+{
+    [TestClass]
+    public class BinaryParseTest
+    {
+        [TestMethod]
+        public void TestExpressionEvaluator()
+        {
+            List<string> expr = ExpEvaluator.ExpressionParser("(7+3) * (5-2)");
+            int expected = 30;
+            int result = ExpEvaluator.Evaluate(expr);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TextExpressionParser()
+        {
+            List<string> lstExpected = new List<string>
+            { "(", "15", "*", "(", "1", "+", "1", ")", ")", "+", "(", "2", "-", "1", ")"};
+
+            List<string> lstOutput = ExpEvaluator.ExpressionParser("(15*(1+1))+(2-1)");
+            for (int i = 0; i < lstExpected.Count; i++)
+            {
+                Assert.AreEqual(lstExpected[i], lstOutput[i]);
+            }
+        }
+    }
+}
